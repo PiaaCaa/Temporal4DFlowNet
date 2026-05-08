a678d292a671860df8551c23b8a1e09000424b97
PiaaCaa <pia.callmer@home.se>
2026-05-08 20:10:52 +0200

    updated input shape of  tuple

--- src/Network/PatchGenerator.py
+++ src/Network/PatchGenerator.py
@@ -4,8 +4,20 @@ from utils import ImageDataset_temporal
 
 class PatchGenerator():
     def __init__(self, patch_size, res_increase, include_all_axis = False, downsample_input_first = True, downsampling_factor = 2):
-        self.patch_size = patch_size
-        self.effective_patch_size = patch_size - 4 # we strip down 2 from each sides (on LR)
+
+        # check instance of patch size
+        if isinstance(patch_size, int):
+            patch_size = (patch_size, patch_size, patch_size)
+            print("Patch size is set to a cube of size", patch_size)
+        elif isinstance(patch_size, tuple):
+            if len(patch_size) != 3:
+                raise ValueError("Patch size must be a tuple of three integers.")
+        else:
+            raise TypeError("Patch size must be an integer or a tuple of three integers.")
+
+        self.patch_size_tuple = patch_size
+        # lower for temporal
+        self.effective_patch_size_tuple = (patch_size[0]-4, patch_size[1]-4,patch_size[2]-4,) # patch_size - 4 # we strip down 2 from each sides (on LR)
         self.res_increase = res_increase
         self.all_axis = include_all_axis
         self.downsample_input_first = downsample_input_first
@@ -62,29 +74,31 @@ class PatchGenerator():
             
             img = img[::self.downsampling_factor, :, :]  # from shape (T, X, Y) to (1/2 T, X, Y) (or other combinations of X; Y; Z)
 
-        side_pad = (self.patch_size-self.effective_patch_size) // 2
+        side_pad_x = (self.patch_size_tuple[0]-self.effective_patch_size_tuple[0]) // 2
+        side_pad_y = (self.patch_size_tuple[1]-self.effective_patch_size_tuple[1]) // 2
+        side_pad_z = (self.patch_size_tuple[2]-self.effective_patch_size_tuple[2]) // 2
         
         # mandatory padding
-        img = np.pad(img, ((0, 0),(side_pad, side_pad),(side_pad, side_pad)), 'constant')
-        img = np.pad(img, ((side_pad, side_pad),(0, 0),(0, 0)), 'wrap')
+        img = np.pad(img, ((0, 0),(side_pad_y, side_pad_y),(side_pad_z, side_pad_z)), 'constant')
+        img = np.pad(img, ((side_pad_x, side_pad_x),(0, 0),(0, 0)), 'wrap')
         
-        res_x = (img.shape[0] % self.effective_patch_size)
-        if (res_x > (2* side_pad)):
-            pad_x = self.patch_size - res_x
+        res_x = (img.shape[0] % self.effective_patch_size_tuple[0])
+        if (res_x > (2* side_pad_x)):
+            pad_x = self.patch_size_tuple[0] - res_x
         else:
-            pad_x = (2 * side_pad) - res_x
+            pad_x = (2 * side_pad_x) - res_x
 
-        res_y = (img.shape[1] % self.effective_patch_size)
-        if (res_y > (2* side_pad)):
-            pad_y = self.patch_size - res_y
+        res_y = (img.shape[1] % self.effective_patch_size_tuple[1])
+        if (res_y > (2* side_pad_y)):
+            pad_y = self.patch_size_tuple[1] - res_y
         else:
-            pad_y = (2 * side_pad) - res_y
+            pad_y = (2 * side_pad_y) - res_y
 
-        res_z = (img.shape[2] % self.effective_patch_size)
-        if (res_z > (2* side_pad)):
-            pad_z = self.patch_size - res_z
+        res_z = (img.shape[2] % self.effective_patch_size_tuple[2])
+        if (res_z > (2* side_pad_z)):
+            pad_z = self.patch_size_tuple[2] - res_z
         else:
-            pad_z = (2 * side_pad) - res_z
+            pad_z = (2 * side_pad_z) - res_z
         
         img = np.pad(img, ((0, pad_x),(0, pad_y),(0, pad_z)), 'constant')
 
@@ -96,26 +110,29 @@ class PatchGenerator():
 
     def _generate_overlapping_patches(self, img):
         
-        patch_size = self.patch_size
+        patch_size = self.patch_size_tuple
         
         img = self._pad_to_patch_size_with_overlap(img)
 
-        all_pads = (self.patch_size - self.effective_patch_size)
+        # all_pads = (self.patch_size_tuple - self.effective_patch_size_tuple)
+        pad_x = self.patch_size_tuple[0] - self.effective_patch_size_tuple[0]
+        pad_y = self.patch_size_tuple[1] - self.effective_patch_size_tuple[1]
+        pad_z = self.patch_size_tuple[2] - self.effective_patch_size_tuple[2]
 
         u_stack = []
         
-        nr_x = (img.shape[0]-all_pads) // self.effective_patch_size
-        nr_y = (img.shape[1]-all_pads) // self.effective_patch_size
-        nr_z = (img.shape[2]-all_pads) // self.effective_patch_size
+        nr_x = (img.shape[0]-pad_x) // self.effective_patch_size_tuple[0]
+        nr_y = (img.shape[1]-pad_y) // self.effective_patch_size_tuple[1]
+        nr_z = (img.shape[2]-pad_z) // self.effective_patch_size_tuple[2]
         
         for i in range(nr_x):
-            x_start = i * self.effective_patch_size #stride x
+            x_start = i * self.effective_patch_size_tuple[0] #stride x
             for j in range(nr_y):
-                y_start = j * self.effective_patch_size #stride y
+                y_start = j * self.effective_patch_size_tuple[1] #stride y
                 for k in range(nr_z):
-                    z_start = k * self.effective_patch_size #stride z
+                    z_start = k * self.effective_patch_size_tuple[2] #stride z
                     
-                    patch_index  = np.index_exp[x_start:x_start+patch_size, y_start:y_start+patch_size, z_start:z_start+patch_size]
+                    patch_index  = np.index_exp[x_start:x_start+patch_size[0], y_start:y_start+patch_size[1], z_start:z_start+patch_size[2]]
                     
                     u_loop = img[patch_index]
                     u_stack.append(u_loop)
@@ -128,19 +145,21 @@ class PatchGenerator():
             Reconstruct the image from the patches
         """
         print("Prediction size:", patches.shape)
+        # TODO check this new outcome
         #patches size: n patches, p1, p2, p3(p1 patches size in 1)
 
-        side_pad = (self.patch_size - self.effective_patch_size) // 2
-        side_pad_hr =  side_pad * self.res_increase # size pad hr = 0
+        side_pad_x = (self.patch_size_tuple[0] - self.effective_patch_size_tuple[0]) // 2
+        side_pax_y = (self.patch_size_tuple[1] - self.effective_patch_size_tuple[1]) // 2
+        side_pad_hr_x =  side_pad_x * self.res_increase # size pad hr = 0
         
-        side_pad_hr_spatial = side_pad # no increase in spatial domain 
+        side_pad_hr_spatial = side_pax_y # no increase in spatial domain 
         patch_size_thr = patches.shape[1]  # patchsize = 20 # temproal high resolution
         patch_size_shr = patches.shape[2]   # spatial high resolution
 
-        n = patch_size_thr-side_pad_hr
+        n = patch_size_thr-side_pad_hr_x
         n_spatial = patch_size_shr - side_pad_hr_spatial
 
-        patches = patches[:,side_pad_hr:n, side_pad_hr_spatial:n_spatial, side_pad_hr_spatial:n_spatial]
+        patches = patches[:,side_pad_hr_x:n, side_pad_hr_spatial:n_spatial, side_pad_hr_spatial:n_spatial]
         
         z_stacks = []
         for k in range(len(patches) // z):
